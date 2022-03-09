a25aa8b1df4b7125954115f9323c7e30622a3d30
hiohayptp <81001539+hiohayptp@users.noreply.github.com>
2022-03-09 13:53:38 +0700

    C: txtUserName -> textboxusername txtPassWord -> textboxmatkhau
    C:
    txtUserName -> textboxusername
    txtPassWord -> textboxmatkhau

--- KiemTra/KiemTra/Program.cs
+++ KiemTra/KiemTra/Program.cs
@@ -16,7 +16,7 @@ namespace KiemTra
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new C());
         }
     }
 }
